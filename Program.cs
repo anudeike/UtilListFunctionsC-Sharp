6839943e4a7b2755d0c4c06810534a1ddb1896ad
anudeike <ikechukwuanude@gmail.com>
2020-03-25 04:54:19 -0500

    creating the utility class

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Util;
+using static Util.ExtendedListFunctions;
+
+
+namespace Util
+{
+    // class UList -> is a utility List, like a regular list but with extra functions
+    public class UList<T> : System.Collections.Generic.List<T>
+    {
+        
+        // how to access members of the list
+        public void Print(string method = "standard", string separator = ",")
+        {
+            switch (method)
+            {
+                case "standard":
+                    foreach (var elem in this)
+                    {
+                        
+                    }
+                    break;
+                case "newline":
+                    foreach (var elem in this)
+                    {
+                        Console.WriteLine(elem);
+                    }
+                    break;
+            }
+        }
+        
+    }
+    
+    class ExtendedListFunctions
+    {
+        /// <summary>
+        /// Gets a slice given the start and end index. Excludes the value of the end index.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"> target list </param>
+        /// <param name="start_index"> start index of the slice</param>
+        /// <param name="end_index"> end index of the slice (exclusive) </param>
+        /// <returns> Desired slice of the original list. Shallow copy.</returns>
+        public static List<T> GetSlice<T>(List<T> list, int start_index, int end_index)
+        {
+            if(list.Count == 0)
+            {
+                throw new System.ArgumentException("List cannot be empty.");
+            }
+
+            if(end_index - start_index < 0)
+            {
+                throw new System.ArgumentException("End Index cannot be less than start index");
+            }
+
+            
+            return list.GetRange(start_index, end_index - start_index);
+        }
+        
+    }
+}
+
+namespace ListUtilityFunctions
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // // get a slice of a List
+            // List<int> example = new List<int>{4, 5, 2, 5, 1, 6, 7};
+            //
+            // List<int> slice = GetSlice(example, 4, 7);
+
+            var utilList = new UList<int> {1, 2, 3, 5};
+            utilList.Print();
+
+            Console.WriteLine("Program Success.");
+        }
+    }
+}
